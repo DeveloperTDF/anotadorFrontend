d29054e55b5251eb4e1a289aba004e108f5bb6f8
tucajas <tucajas@gmail.com>
2024-09-16 13:08:37 -0300

    tercer commit

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -1,7 +1,7 @@
 import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
@@ -15,7 +15,8 @@ import { ComponentsModule } from '../components/components.module';
     FormsModule,
     ExploreContainerComponentModule,
     Tab2PageRoutingModule,
-    ComponentsModule
+    ComponentsModule,
+    ReactiveFormsModule
   ],
   declarations: [Tab2Page]
 })
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  formulario!: FormGroup;
+  
+  constructor( private formBuilder: FormBuilder) {}
 
+  ngOnInit() {
+    this.formulario = this.formBuilder.group({
+      fecha: ['', [Validators.required]],
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      temario:['', [Validators.required, Validators.minLength(3)]],
+      titulo: [ '', [Validators.required, Validators.minLength(3)]],
+      apunte: ['', [Validators.required, Validators.maxLength(300)]]
+    });
+  }
+  onSubmit(){
+    if (this.formulario.valid) {
+       const { fecha, nombre,temario, titulo,apunte } = this.formulario.value;
+       console.log('fecha:', fecha);
+       console.log('nombre:', nombre);
+       console.log('temario:', temario);
+       console.log('titulo:', titulo);
+       console.log('apunte:', apunte);
+      // Aquí puedes implementar la lógica para iniciar sesión
+      
+    } else {
+      console.log('Formulario inválido');
+    }
+  }
 }
